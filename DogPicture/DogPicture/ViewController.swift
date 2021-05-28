ad8d6946ca74eee7c8ed8c35c89e46ac3758b6e0
ornwoo96 <dongou705@naver.com>
2021-05-28 15:40:24 +0900

    Project Init

---
+++ DogPicture/DogPicture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogPicture
+//
+//  Created by 김동우 on 2021/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
