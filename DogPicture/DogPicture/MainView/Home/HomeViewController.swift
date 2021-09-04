b0b0b4944f70cf978903588011488cec15b4bb2a
ornwoo96 <dongou705@naver.com>
2021-09-04 21:11:11 +0900

    feat : HomeView Sticky Header 미완2

--- DogPicture/DogPicture/MainView/Home/HomeViewController.swift
+++ DogPicture/DogPicture/MainView/Home/HomeViewController.swift
@@ -19,10 +19,6 @@ class HomeViewController: UIViewController {
                                  "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
     
     var homeTableView = UITableView()
-    
-    var homeTableViewFooter = UITableViewHeaderFooterView()
-    var headerContentLabel = UILabel()
-    var headerDateLabel = UILabel()
     let models = ["쁘디1", "쁘띠2", "쁘띠3", "쁘띠4", "쁘띠5",
                   "쁘띠6", "쁘띠7", "쁘띠8", "쁘띠9", "쁘띠10",
                   "쁘띠11", "쁘띠12", "쁘띠13", "쁘띠14", "쁘띠15",
@@ -34,6 +30,8 @@ class HomeViewController: UIViewController {
     var selectroViewColor: UIColor = .red
     var selectorTextColor: UIColor = .red
     
+    let homeTableHeaderView = UIScrollView()
+    
     
     var tagNumber = 0
     
@@ -44,48 +42,30 @@ class HomeViewController: UIViewController {
         navigationController?.isNavigationBarHidden = true
         
 //        segmentedUpdateView()
+        updateView()
         
+    }
+    
+    func updateView() {
         attribute()
         layout()
     }
     
     func attribute() {
         view.addSubview(homeTableView)
-        
+        homeTableHeaderView.addSubview(segmentedScrollContentView)
+        segmentedScrollContentView.addSubview(segmentedStackView)
         
         homeTableView.do {
             $0.dataSource = self
             $0.delegate = self
+            $0.tableHeaderView = homeTableHeaderView
             $0.frame = view.bounds
             $0.backgroundColor = .systemBlue
             $0.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-//            $0.register(HomeViewSegmentedHeader.self, forHeaderFooterViewReuseIdentifier: "SegmentedHeader")
-//            $0.tableHeaderView = header
         }
-    }
-    
-    func layout() {
-        homeTableView.do {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-            $0.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        }
-    }
-    
-    func segmentedUpdateView() {
-        attributeSegmentedControl()
-        configSegmentedControl()
-    }
-    
-    func attributeSegmentedControl() {
-        view.addSubview(segmentedScrollView)
-        segmentedScrollView.addSubview(segmentedScrollContentView)
-        segmentedScrollContentView.addSubview(segmentedStackView)
-        
-        segmentedScrollView.do {
-            $0.backgroundColor = .systemBlue
+        homeTableHeaderView.do {
+            $0.backgroundColor = .systemPink
             $0.contentSize = CGSize(width: 840, height: 70)
         }
         
@@ -113,21 +93,30 @@ class HomeViewController: UIViewController {
         segmentedButton[0].setTitleColor(selectorTextColor, for: .normal)
     }
     
-    func configSegmentedControl() {
-        segmentedScrollView.do {
+    func layout() {
+        homeTableView.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
             $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
             $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            $0.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        }
+        headerViewLayout()
+    }
+    
+    func headerViewLayout() {
+        homeTableHeaderView.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
             $0.heightAnchor.constraint(equalToConstant: 70).isActive = true
         }
         
         segmentedScrollContentView.do {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            $0.topAnchor.constraint(equalTo: segmentedScrollView.safeAreaLayoutGuide.topAnchor).isActive = true
-            $0.leadingAnchor.constraint(equalTo: segmentedScrollView.leadingAnchor).isActive = true
-            $0.bottomAnchor.constraint(equalTo: segmentedScrollView.frameLayoutGuide.bottomAnchor).isActive = true
-            $0.widthAnchor.constraint(equalToConstant: segmentedScrollView.contentSize.width).isActive = true
+            $0.topAnchor.constraint(equalTo: homeTableHeaderView.safeAreaLayoutGuide.topAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: homeTableHeaderView.leadingAnchor).isActive = true
+            $0.bottomAnchor.constraint(equalTo: homeTableHeaderView.frameLayoutGuide.bottomAnchor).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: homeTableHeaderView.contentSize.width).isActive = true
         }
         
         segmentedStackView.do {
@@ -138,6 +127,71 @@ class HomeViewController: UIViewController {
             $0.heightAnchor.constraint(equalToConstant: 50).isActive = true
         }
     }
+    
+//    func segmentedUpdateView() {
+//        attributeSegmentedControl()
+//        configSegmentedControl()
+//    }
+//
+//    func attributeSegmentedControl() {
+//        view.addSubview(segmentedScrollView)
+//        segmentedScrollView.addSubview(segmentedScrollContentView)
+//        segmentedScrollContentView.addSubview(segmentedStackView)
+//
+//        segmentedScrollView.do {
+//            $0.backgroundColor = .systemBlue
+//            $0.contentSize = CGSize(width: 840, height: 70)
+//        }
+//
+//        segmentedStackView.do {
+//            $0.backgroundColor = .systemGreen
+//            $0.axis = .horizontal
+//            $0.alignment = .fill
+//            $0.distribution = .fillEqually
+//        }
+//
+//        for buttonTitles in segmentedButtonTitles {
+//            let button = UIButton(type: .system)
+//            button.do {
+//                $0.tag = tagNumber
+//                $0.setTitle(buttonTitles, for: .normal)
+//                $0.addTarget(self,
+//                             action: #selector(segmentLineAnimation(sender:)),
+//                             for: .touchUpInside)
+//                $0.setTitleColor(textColor, for: .normal)
+//            }
+//            segmentedStackView.addArrangedSubview(button)
+//            segmentedButton.append(button)
+//            tagNumber += 1
+//        }
+//        segmentedButton[0].setTitleColor(selectorTextColor, for: .normal)
+//    }
+//
+//    func configSegmentedControl() {
+//        segmentedScrollView.do {
+//            $0.translatesAutoresizingMaskIntoConstraints = false
+//            $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+//            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+//            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+//            $0.heightAnchor.constraint(equalToConstant: 70).isActive = true
+//        }
+//
+//        segmentedScrollContentView.do {
+//            $0.translatesAutoresizingMaskIntoConstraints = false
+//            $0.topAnchor.constraint(equalTo: segmentedScrollView.safeAreaLayoutGuide.topAnchor).isActive = true
+//            $0.leadingAnchor.constraint(equalTo: segmentedScrollView.leadingAnchor).isActive = true
+//            $0.bottomAnchor.constraint(equalTo: segmentedScrollView.frameLayoutGuide.bottomAnchor).isActive = true
+//            $0.widthAnchor.constraint(equalToConstant: segmentedScrollView.contentSize.width).isActive = true
+//        }
+//
+//        segmentedStackView.do {
+//            $0.translatesAutoresizingMaskIntoConstraints = false
+//            $0.topAnchor.constraint(equalTo: segmentedScrollContentView.topAnchor).isActive = true
+//            $0.leadingAnchor.constraint(equalTo: segmentedScrollContentView.leadingAnchor).isActive = true
+//            $0.widthAnchor.constraint(equalToConstant: CGFloat(segmentedButtonTitles.count*70)).isActive = true
+//            $0.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//        }
+//    }
     
     @objc func segmentLineAnimation(sender: UIButton) {
         presenter?.calenderDidTap(tag: sender.tag)
@@ -155,18 +209,14 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
         cell.textLabel?.text = models[indexPath.row]
         return cell
     }
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        let header = HomeViewStickyHeaderView()
-//        header.randomImageView.image = UIImage(named: "chu3")
-        
-        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "StickyHeader")
-        return header
-    }
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let header = homeTableHeaderView
+//        return header
+//    }
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 100
