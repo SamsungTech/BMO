2a3a1350944377574dd8fa5eaa175057a4a33a60
ornwoo96 <dongou705@naver.com>
2021-10-01 17:46:11 +0900

    feat: SettingViewController 설정 Item들 다 추가 완, view에 안뜸 현상 미해결

--- DogPicture/DogPicture/Setting/SettingView/SettingViewController.swift
+++ DogPicture/DogPicture/Setting/SettingView/SettingViewController.swift
@@ -17,6 +17,7 @@ class SettingViewController: UIViewController {
     
     var profileView = UIView()
     var profileImage = UIImageView()
+    let profileEditButton = UIButton()
     var dogType = UILabel()
     var dogName = UILabel()
     
@@ -32,10 +33,12 @@ class SettingViewController: UIViewController {
     var weight = UILabel()
     var dogWeightImage = UIImageView()
     
-    let profileEditButton = UIButton()
+    var settingHeaderLabel = UILabel()
     let pushNotificationButton = UIButton()
+    let dogTypeEditButton = UIButton()
     let termsOfUseButton = UIButton()
     let reviewButton = UIButton()
+    let eventButton = UIButton()
     var versionInfoButton = UIButton()
     
     override func viewDidLoad() {
@@ -55,12 +58,11 @@ class SettingViewController: UIViewController {
     func attribute() {
         view.addSubview(verticalStackView)
         [ backgroundView, settingTitle ].forEach() { verticalStackView.addSubview($0) }
-        backgroundView.addSubview(profileView)
-        [ profileImage, dogName, dogType, profileEditButton,
-          dogAgeImage, dogAge, dogAgeDays,
-          dogGenderImage, dogGender, neutering,
-          dogWeightImage, dogWeight, weight ]
-            .forEach() { profileView.addSubview($0) }
+        [ profileView, settingHeaderLabel, pushNotificationButton, dogTypeEditButton,
+          eventButton, termsOfUseButton, reviewButton, versionInfoButton ].forEach() { backgroundView.addSubview($0) }
+        [ profileImage, dogName, dogType, profileEditButton, dogAgeImage, dogAge,
+          dogAgeDays, dogGenderImage, dogGender, neutering, dogWeightImage, dogWeight, weight ].forEach() { profileView.addSubview($0) }
+        
         
         verticalStackView.do {
             $0.backgroundColor = .systemPink
@@ -152,6 +154,34 @@ class SettingViewController: UIViewController {
             $0.text = "2.3kg"
             $0.textColor = .darkGray
             $0.font = UIFont.systemFont(ofSize: 12)
+        }
+//        pushNotificationButton, dogTypeEditButton, eventButton, termsOfUseButton, reviewButton, versionInfoButton
+        settingHeaderLabel.do {
+            $0.text = "쁘띠오빠의 설정"
+            $0.textColor = .black
+            $0.font = UIFont.boldSystemFont(ofSize: 15)
+        }
+        pushNotificationButton.do {
+            $0.setImage(UIImage(systemName: "bell.fill"), for: .normal)
+            $0.tintColor = .systemPink
+        }
+        dogTypeEditButton.do {
+            $0.backgroundColor = .white
+            $0.tintColor = .white
+            $0.setTitle("타입설정", for: .normal)
+        }
+        eventButton.do {
+            $0.backgroundColor = .white
+            
+        }
+        termsOfUseButton.do {
+            $0.backgroundColor = .white
+        }
+        reviewButton.do {
+            $0.backgroundColor = .white
+        }
+        versionInfoButton.do {
+            $0.backgroundColor = .white
         }
     }
     func layout() {
@@ -262,6 +292,55 @@ class SettingViewController: UIViewController {
             $0.topAnchor.constraint(equalTo: dogWeight.bottomAnchor, constant: 10).isActive = true
             $0.centerXAnchor.constraint(equalTo: dogWeightImage.centerXAnchor).isActive = true
         }
-        
+        //pushNotificationButton, dogTypeEditButton, eventButton, termsOfUseButton, reviewButton, versionInfoButton
+        settingHeaderLabel.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: profileView.bottomAnchor, constant: 15).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        }
+        pushNotificationButton.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: settingHeaderLabel.bottomAnchor, constant: 15).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.trailingAnchor, constant: 15).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 107.5).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        }
+        dogTypeEditButton.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: settingHeaderLabel.bottomAnchor, constant: 15).isActive = true
+            $0.leadingAnchor.constraint(equalTo: pushNotificationButton.trailingAnchor, constant: 15).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 107.5).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        }
+        eventButton.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: settingHeaderLabel.bottomAnchor, constant: 15).isActive = true
+            $0.leadingAnchor.constraint(equalTo: dogTypeEditButton.trailingAnchor, constant: 15).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 107.5).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        }
+        termsOfUseButton.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: pushNotificationButton.bottomAnchor, constant: 15).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.trailingAnchor, constant: 15).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 107.5).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        }
+        reviewButton.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: pushNotificationButton.bottomAnchor, constant: 15).isActive = true
+            $0.leadingAnchor.constraint(equalTo: termsOfUseButton.trailingAnchor, constant: 15).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 107.5).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        }
+        versionInfoButton.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: pushNotificationButton.bottomAnchor, constant: 15).isActive = true
+            $0.leadingAnchor.constraint(equalTo: reviewButton.trailingAnchor, constant: 15).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 107.5).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        }
     }
 }
